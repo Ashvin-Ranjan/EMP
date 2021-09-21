70e4bf0af00c40e672e8109c5465352b0f0da1ef
Ashvin-Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2021-09-21 14:44:25 -0700

    Make the main create a cli

---
+++ src/argument.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::string::String;
+
+pub enum ArgumentOptions {
+  ReadFromFile(String),
+  FromJSON(String),
+  Help,
+}
+
+pub fn resolve_arguments(_a: env::Args) -> ArgumentOptions {
+  let mut args = _a;
+  match args.nth(1) {
+    Some(a) => match &a[..] {
+      "-r" | "--read" => {
+        if let Some(file) = args.nth(0) {
+          return ArgumentOptions::ReadFromFile(file);
+        }
+        return ArgumentOptions::Help;
+      }
+      "-fj" | "--from_json" => {
+        if let Some(json) = args.nth(0) {
+          return ArgumentOptions::FromJSON(json);
+        }
+        return ArgumentOptions::Help;
+      }
+      _ => return ArgumentOptions::Help,
+    },
+    None => return ArgumentOptions::Help,
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
+mod argument;
 pub mod constants;
 pub mod decode;
 pub mod encode;
 pub mod errors;
 pub mod value;
 
+use argument::ArgumentOptions;
+use serde_json;
+use std::env;
 use std::fs;
-use std::io::Read;
 
 fn main() {
-  let mut obj = std::collections::HashMap::new();
-
-  obj.insert("index2".to_owned(), value::Value::Float(2.43));
-
-  if let Ok(readata) = fs::read_to_string("./test.json") {
-    let data = encode::json::encode_json(serde_json::from_str(&readata).unwrap());
-
-    fs::write("encode.emp", &data).expect("Unable to write file");
-
-    let mut d = std::vec::Vec::new();
-
-    let mut f = fs::File::open("encode.emp").expect("Unable to read file");
-
-    f.read_to_end(&mut d).expect("Cannot read");
-
-    let out = decode::json::decode_json_unsafe(&d);
-
-    match out {
-      Ok(o) => println!("Extracted data: {}", o),
-      Err(e) => println!("Error: {}", e),
+  match argument::resolve_arguments(env::args()) {
+    ArgumentOptions::ReadFromFile(file) => match fs::read(file) {
+      Ok(data) => {
+        match decode::decode(&data) {
+          Ok((v, _)) => println!("Extracted data: {}", v),
+          Err(v) => println!("An error occured while decoding: {}", v),
+        };
+      }
+      Err(_) => println!("Unable to read file (Are you sure it exists?)"),
+    },
+    ArgumentOptions::FromJSON(json) => match serde_json::from_str::<serde_json::Value>(&json[..]) {
+      Ok(json) => {
+        println!("{}", value::json::from_json(json))
+      }
+      Err(_) => println!("Unable to parse JSON Data (Did you put it in quotes?)"),
+    },
+    ArgumentOptions::Help => {
+      println!("┌───────────────────────────────────┐");
+      println!("│[E]fficiently [M]anaged [P]ackaging│");
+      println!("│               Help:               │");
+      println!("│[-r | --read] <filename>: Reads the│");
+      println!("│EMP bytecode and prints it out as a│");
+      println!("│EMP string.                        │");
+      println!("│                                   │");
+      println!("│[-fj | --from_json] <json>: Parses │");
+      println!("│the JSON data and prints it out as │");
+      println!("│an EMP string.                     │");
+      println!("│                                   │");
+      println!("│NOTE: Make sure your json data is  │");
+      println!("│in quotes                          │");
+      println!("└───────────────────────────────────┘");
     }
   }
 }
